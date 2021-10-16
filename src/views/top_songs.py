0f0e9ee80b500429512e632eaa86b54dc3d126d6
Renan Dantas <dantas.renan@icloud.com>
2021-10-16 19:16:47 -0300

    feat: adds persistence to redis on songs

--- src/views/top_songs.py
+++ src/views/top_songs.py
@@ -1,15 +1,17 @@
 import unidecode
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, inputs, reqparse
 
-from services import dynamo_db, genius_api
+from helpers.helpers import Helpers
+from services import dynamo_db, genius_api, redis_instance
 
 
 class TopSongs(Resource):
     genius_api = genius_api.GeniusApi()
     dynamo_db = dynamo_db.DynamoDb("artists")
+    redis = redis_instance.Redis()
 
     parser = reqparse.RequestParser()
-    parser.add_argument("cache", type=bool, default=True)
+    parser.add_argument("cache", type=inputs.boolean, default=True)
     parser.add_argument("artist", type=str, required=True)
 
     def get(self):
@@ -17,12 +19,15 @@ class TopSongs(Resource):
         artist = unidecode.unidecode(
             request_data["artist"]
         ).lower()  # remove accents and transform to lowercase
-        cache = request_data["cache"]
 
+        cache = request_data["cache"]
         artist_data = self.dynamo_db.get("artist_name", artist)
 
         if cache and artist_data:
-            return {"artist": artist, "top_songs": artist_data["songs"]}, 200
+            cache_data = self.redis.get_list(artist_data["transaction_id"])
+            songs_sorted = Helpers.create_enumerate_song_list(cache_data)
+
+            return {"artist": artist, "top_songs": songs_sorted}, 200
 
         response = self.genius_api.get_top_songs(artist)
 
@@ -34,10 +39,11 @@ class TopSongs(Resource):
 
         songs = response["message"]
 
-        self.dynamo_db.create(
+        transaction_id = self.dynamo_db.create(
             key_name="artist_name", key_value=artist, songs=songs
         )
 
-        top_songs = {idx + 1: value for (idx, value) in enumerate(songs)}
+        self.redis.manage_lists(transaction_id, songs)
+        songs_sorted = Helpers.create_enumerate_song_list(songs)
 
-        return {"artist": artist, "top_songs": top_songs}, 200
+        return {"artist": artist, "top_songs": songs_sorted}, 200
